904f8f77a643d04a547702ca9de381d336b50772
Eugene Pisarev <zm1s3ry@gmail.com>
2025-01-12 13:48:41 +0300

    add db model for complaints

--- property/admin.py
+++ property/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Flat
+from .models import Flat, Complaint
 
 
-class SiteAdmin(admin.ModelAdmin):
+@admin.register(Flat)
+class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'town_district', 'address')
     readonly_fields = ['created_at']
     list_display = ['address', 'price', 'new_building', 'construction_year', 'town']
@@ -11,6 +12,10 @@ class SiteAdmin(admin.ModelAdmin):
     list_filter = ['new_building', 'rooms_number', 'has_balcony']
 
 
-admin.site.register(Flat, SiteAdmin)
+@admin.register(Complaint)
+class ComplaintAdmin(admin.ModelAdmin):
+    raw_id_fields = ['user', 'flat']
+    list_display = ['user', 'flat', 'created_at']
+    search_fields = ['user__username', 'flat__address', 'message']
 
 
--- property/models.py
+++ property/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Flat(models.Model):
@@ -62,3 +63,14 @@ class Flat(models.Model):
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
+
+
+class Complaint(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Кто жаловался', related_name='complaints')
+    flat = models.ForeignKey('property.Flat', on_delete=models.CASCADE, verbose_name='Квартира, на которую жаловались',
+                             related_name='complaints')
+    message = models.TextField('Текст жалобы', help_text='Опишите вашу жалобу')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+    def __str__(self):
+        return f'Жалоба от {self.user.username} на {self.flat.address}'
